4aa3a4408a695f2c03b5ccf94a6c3de2910d8eb6
exe <ayoubarid000@gmail.com>
2025-10-03 22:46:32 +0100

    Set the hover random color for each square
    Implement a css custom priority to set the random
    rbg values function, And set for each div a listener
    on hover to get the random rbg values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
+// Random RBG from hover color
+function getRandomRGBValues() {
+	const r = Math.floor(Math.random() * 250);
+	const g = Math.floor(Math.random() * 250);
+	const b = Math.floor(Math.random() * 250);
+	return (`rgb(${r},${g},${b})`);
+}
+
+// Generate the divs and append them to container
 function createGrid(size) {
 	for (let i = 0; i < size * size; i++) {
 		const div = document.createElement("div");
+		// Add event listener to change color on hover
+		div.addEventListener("mouseover", function() {
+			div.style.backgroundColor = getRandomRGBValues();
+		})
 		container.appendChild(div);
 	}
 }
-
 const container = document.getElementById("container");
-
+// Create Default grid with 16x16
 let size = 16;
 container.style.setProperty('--size', size);
 createGrid(size);
+
+// Lisen to button to get user input size
 const button = document.querySelector("button");
 button.addEventListener("click", function() {
 	size = Number(prompt("Enter the size of the grid (max 100);"));
